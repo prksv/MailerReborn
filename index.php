427d3991fe68a82a31cb12373dc3f9a9e23a83a6
prskv <doodking.adv@gmail.com>
2022-01-21 21:36:51 +0500

    master

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 require 'classes/MailerReborn.php';
 
-$token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IlNjY3NyUHIwYW9vZmVPZk4wTEp6UDNtY3hUZHJkOHByMDJ6SFZpTXNCREtZU0ZEbUFEWUdvT3kzbEdyNE1TZHgxVUltWUROaWw2b3I1YzZ1NmM2WVZWcm1nQ1J2Tmx6ZkVrU3hBcTJVdVpIa1lydGgwOXdiOUluWXF4b1BIQm9YIiwidXNlcklkIjoxMjkyMDMyOTc2LCJpYXQiOjE2NDI3Nzc1NTF9.yCz9QbbUmgTyd00Cd0gOXRBSG4fbnEcj9GtW3i1G7BM';
+$token = '';
 $MailerReborn = new MailerReborn($token);
 
                                                             
@@ -41,7 +41,7 @@ $data = [
 
 // И наконец получаем ответ. Работает со всеми методами.
 
-$MailerReborn->sendSMS('79697124393', 'f5a9dd69-377e-4597-a642-fd2331227344', 'Это сообщение отправлено с кайфом');
+$MailerReborn->sendSMS('номер', 'f5a9dd69-377e-4597-a642-fd2331227344', 'Это сообщение отправлено с кайфом');
 
 
 
